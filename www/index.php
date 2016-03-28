ec74171deb46dc9d287c0af1e5c6c0b47157ce46
Facedown <klapuchdominik@gmail.com>
2016-03-28 21:48:29 +0200

    workaround when form was send with :80 port

--- www/index.php
+++ www/index.php
@@ -1,5 +1,8 @@
 <?php
 declare(strict_types = 1);
 
+if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+    $_SERVER['SERVER_PORT'] = 443;
+
 $container = require __DIR__ . '/../app/bootstrap.php';
 $container->getByType('Nette\Application\Application')->run();
