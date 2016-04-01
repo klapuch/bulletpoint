7055a5eb6b5cd8a69900f56f73c1b107f4416d8e
Facedown <klapuchdominik@gmail.com>
2016-04-01 18:31:35 +0200

    Optimized autoloading

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,17 +3,18 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
-
 $configurator->setDebugMode(true);
 $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
-$configurator->createRobotLoader()
-    ->addDirectory(__DIR__)
-    ->register();
-
-$configurator->addConfig(__DIR__ . '/Config/config.neon', Nette\Configurator::AUTO);
-$configurator->addConfig(__DIR__ . '/Config/config.local.neon', Nette\Configurator::AUTO);
+$configurator->addConfig(
+    __DIR__ . '/Config/config.neon',
+    Nette\Configurator::AUTO
+);
+$configurator->addConfig(
+    __DIR__ . '/Config/config.local.neon',
+    Nette\Configurator::AUTO
+);
 $container = $configurator->createContainer();
 
 return $container;
