f4d61e55bf69f4cc4c2a5f7db701b72f360ffcaf
Facedown <klapuchdominik@gmail.com>
2016-04-03 00:01:08 +0200

    Named type of query and set length

--- app/Model/Storage/ProfiledDatabase.php
+++ app/Model/Storage/ProfiledDatabase.php
@@ -11,31 +11,35 @@ final class ProfiledDatabase implements Database {
     }
 
     public function fetch(string $query, array $parameters = []) {
-        $this->dump($query);
+        $this->dump($query, __FUNCTION__);
         return $this->origin->fetch($query, $parameters);
     }
 
     public function fetchAll(string $query, array $parameters = []) {
-        $this->dump($query);
+        $this->dump($query, __FUNCTION__);
         return $this->origin->fetchAll($query, $parameters);
     }
 
     public function fetchColumn(string $query, array $parameters = []) {
-        $this->dump($query);
+        $this->dump($query, __FUNCTION__);
         return $this->origin->fetchColumn($query, $parameters);
     }
 
     public function query(string $query, array $parameters = []) {
-        $this->dump($query);
+        $this->dump($query, __FUNCTION__);
         return $this->origin->query($query, $parameters);
     }
 
     public function exec(string $query) {
-        $this->dump($query);
+        $this->dump($query, __FUNCTION__);
         $this->origin->exec($query);
     }
 
-    private function dump($query) {
-        Tracy\Debugger::barDump($query, 'Query');
+    private function dump($query, $type) {
+        Tracy\Debugger::barDump(
+            $query,
+            $type,
+            [Tracy\Dumper::TRUNCATE => 1000,]
+        );
     }
 }
