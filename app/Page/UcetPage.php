67a29b76ee82554e4fb05810d94406963aeb5ca1
Facedown <klapuchdominik@gmail.com>
2016-04-23 20:56:00 +0200

    form to change photo moved to own class

--- app/Page/UcetPage.php
+++ app/Page/UcetPage.php
@@ -32,37 +32,8 @@ final class UcetPage extends BasePage {
         $this->template->email = $this->account->email();
     }
 
-    protected function createComponentPhotoChangeForm() {
-        $form = new Component\BaseForm();
-        $form->addProtection();
-        $form->addUpload('photo', 'Soubor')
-            ->addRule(UI\Form::FILLED, '%label musí být vybrán')
-            ->addRule(UI\Form::MAX_FILE_SIZE, 'Fotka nesmí přesahovat 500 kB.', 500 * 1024);
-        $form->onSuccess[] = function() {
-            $this->photoChangeFormSucceeded();
-        };
-        $form->addSubmit('change');
-        return $form;
-    }
-
-    public function photoChangeFormSucceeded() {
-        try {
-            (new User\ProfilePhoto(
-                $this->identity,
-                new Filesystem\Folder(Paths::profileImage())
-            ))->change(
-                new Filesystem\Image(
-                    new Filesystem\UploadedFile($_FILES['photo'])
-                )
-            );
-            $this->flashMessage('Fotka je změněna', 'success');
-            $this->flashMessage('Obnov stránku pro změnu', 'warning');
-            $this->redirect('this');
-        } catch(Exception\UploadException $ex) {
-            $this->flashMessage($ex->getMessage(), 'danger');
-        } catch(Exception\StorageException $ex) {
-            $this->flashMessage($ex->getMessage(), 'danger');
-        }
+    protected function createComponentPhotoForm() {
+        return new Component\PhotoForm($this->identity);
     }
 
     protected function createComponentPasswordChangeForm() {
