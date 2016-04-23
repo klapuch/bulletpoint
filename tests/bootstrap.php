c97f77e1996a82aa65b4031bd6d0ecd3df22d91b
Facedown <klapuchdominik@gmail.com>
2016-04-23 18:11:47 +0200

    merged testbench bootstrap with common bootstrap for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,8 +9,19 @@ date_default_timezone_set('Europe/Prague');
 $configurator = new Nette\Configurator;
 $configurator->setDebugMode(false);
 $configurator->setTempDirectory(__DIR__ . '/temp');
+$configurator->addConfig(__DIR__ . '/../app/config/config.neon', 'test');
 $configurator->createRobotLoader()
     ->addDirectory(__DIR__)
     ->register();
+
+Testbench\Bootstrap::setup(
+    __DIR__ . '/temp_testbench',
+    function(Nette\Configurator $configurator) {
+        $configurator->addConfig(
+            __DIR__ . '/../app/config/config.neon',
+            'test'
+        );
+    }
+);
 
 return $configurator->createContainer();
