40d6dda4fc13ffa71d37da8b2984d2d011f0d672
Facedown <klapuchdominik@gmail.com>
2016-04-28 01:11:36 +0200

    logIn and logOut added to Component test case

--- tests/TestCase/Component.php
+++ tests/TestCase/Component.php
@@ -14,9 +14,33 @@ abstract class Component extends \Tester\TestCase {
 
     public function setUp() {
         parent::setUp();
+        $this->logOut();
         \Tester\Environment::lock('component', __DIR__ . '/../temp');
         $this->container
             ->getByType('Bulletpoint\Model\Storage\Database')
             ->exec(file_get_contents(__DIR__ . '/../../dataset.sql'));
+    }
+
+    protected function logIn($id = 1, $roles = null, $data = null) {
+        if($id instanceof \Nette\Security\IIdentity) {
+            $identity = $id;
+        } else {
+            $identity = new \Nette\Security\Identity($id, $roles, $data);
+        }
+        /** @var \Nette\Security\User $user */
+        $user = \Testbench\ContainerFactory::create(false)->getByType(
+            'Nette\Security\User'
+        );
+        $user->login($identity);
+        return $user;
+    }
+
+    protected function logOut() {
+        /** @var \Nette\Security\User $user */
+        $user = \Testbench\ContainerFactory::create(false)->getByType(
+            'Nette\Security\User'
+        );
+        $user->logout();
+        return $user;
     }
 }
