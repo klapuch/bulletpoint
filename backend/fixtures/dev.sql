fd38b22b1ad7c71588d3151cd4bd32bdd4258e50
Dominik Klapuch <klapuchdominik@gmail.com>
2018-12-08 19:04:38 +0100

    array as tags

--- backend/fixtures/dev.sql
+++ backend/fixtures/dev.sql
@@ -6,5 +6,5 @@ INSERT INTO sources (link, type) VALUES ('https://www.wikipedia.com/php', 'web')
 INSERT INTO tags (name) VALUES ('programming language');
 INSERT INTO tags (name) VALUES ('IT');
 
-INSERT INTO themes (name, tags, reference_id, user_id) VALUES ('PHP', json_build_array(ARRAY[1, 2]), 1, 1);
+INSERT INTO themes (name, tags, reference_id, user_id) VALUES ('PHP', '[1,2]', 1, 1);
 INSERT INTO bulletpoints (theme_id, source_id, user_id, text) VALUES (1, 1, 1, 'Good');
