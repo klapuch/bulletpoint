a725eaa23a456a3b185c0bde47f0f314a156ca97
Dominik Klapuch <klapuchdominik@gmail.com>
2018-12-22 10:43:03 +0100

    Loader with delay

--- frontend/src/ui/Loader.js
+++ frontend/src/ui/Loader.js
@@ -2,8 +2,34 @@
 import React from 'react';
 import Center from '../components/Center';
 
-export default () => (
-  <Center>
-    <h1>Načítám...</h1>
-  </Center>
-);
+type Props = *;
+type State = {|
+  +show: boolean,
+|};
+export default class extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { show: false };
+    this.timer = setTimeout(this.showMessage, 250);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  showMessage = () => this.setState({ show: true });
+
+  timer: any;
+
+  render() {
+    const { show } = this.state;
+    if (show === true) {
+      return (
+        <Center>
+          <h1>Načítám...</h1>
+        </Center>
+      );
+    }
+    return null;
+  }
+}
