de6fc188076cf7065886a37847817d19a94b338b
Dominik Klapuch <klapuchdominik@gmail.com>
2018-12-25 13:14:27 +0100

    better default values in dev.sql

--- backend/fixtures/dev.sql
+++ backend/fixtures/dev.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (email, password, role) VALUES ('klapuchdominik@gmail.com', '$argon2i$v=19$m=131072,t=4,p=4$ZEhjRjVaYzYuYkg2VUQwcQ$EuRHYiI+7NJoVtkcdbPTh4QUHxWw7FOncPphl18ZGq4', 'member'); -- heslo123
 UPDATE access.verification_codes SET used_at = now() WHERE user_id = 1;
 
-INSERT INTO "references" (url) VALUES ('https://www.wikipedia.com');
-INSERT INTO sources (link, type) VALUES ('https://www.wikipedia.com/php', 'web');
+INSERT INTO "references" (url) VALUES ('https://wikipedia.org/wiki/PHP');
+INSERT INTO sources (link, type) VALUES ('https://wikipedia.org/wiki/PHP', 'web');
 
 INSERT INTO tags (name) VALUES ('programming language');
 INSERT INTO tags (name) VALUES ('IT');
@@ -11,4 +11,4 @@ INSERT INTO themes (name, reference_id, user_id) VALUES ('PHP', 1, 1);
 
 INSERT INTO theme_tags (theme_id, tag_id) VALUES (1, 1), (1, 2);
 
-INSERT INTO bulletpoints (theme_id, source_id, user_id, content) VALUES (1, 1, 1, 'Good');
+INSERT INTO bulletpoints (theme_id, source_id, user_id, content) VALUES (1, 1, 1, 'Strmá křivka učení');
