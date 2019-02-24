54b3282874b98cf1072eb7704aaa177bd62b14ff
Dominik Klapuch <klapuchdominik@gmail.com>
2019-02-24 23:46:44 +0100

    better migration script

--- backend/migrations/run.sh
+++ backend/migrations/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-psql -h localhost -U bulletpoint --single-transaction bulletpoint < $1
+psql -h localhost -U bulletpoint --single-transaction -d bulletpoint -v ON_ERROR_STOP=1 -X --file $1
