63d6d70d21bd4b98eac78b3870db59342f74afe0
Dominik Klapuch <klapuchdominik@gmail.com>
2019-03-10 20:39:16 +0100

    WIP

--- backend/Tests/Integration/Domain/Access/FacebookEntranceTest.php
+++ backend/Tests/Integration/Domain/Access/FacebookEntranceTest.php
@@ -3,15 +3,11 @@ declare(strict_types = 1);
 
 namespace Bulletpoint\Integration\Domain\Access;
 
-use Bulletpoint\Domain\Access\ConstantUser;
-use Bulletpoint\Domain\Access\FacebookEntrance;
-use Bulletpoint\Http;
+use Bulletpoint\Domain\Access;
 use Bulletpoint\TestCase;
-use GuzzleHttp\Client;
-use Klapuch\Storage\Connection;
-use Klapuch\Storage\NativeQuery;
-use Klapuch\Storage\TypedQuery;
-use Klapuch\Uri;
+use GuzzleHttp;
+use Klapuch\Storage;
+use Mockery\MockInterface;
 use Nette\Utils\Json;
 use Psr\Http\Message\ResponseInterface;
 use Tester\Assert;
@@ -31,22 +27,24 @@ final class FacebookEntranceTest extends TestCase\Runtime {
 	 * @throws \UnexpectedValueException Error during retrieving Facebook token.
 	 */
 	public function testThrowingOnNotOkStatus(): void {
-		$http = $this->mock(Client::class);
+		/** @var GuzzleHttp\Client|MockInterface $http */
+		$http = $this->mock(GuzzleHttp\Client::class);
 		$response = $this->mock(ResponseInterface::class);
 		$response->shouldReceive('getStatusCode')->andReturn(HTTP_BAD_REQUEST);
 		$response->shouldReceive('getBody')->andReturn('');
 		$http->shouldReceive('request')->andReturn($response);
-		(new FacebookEntrance($this->connection, $http))->enter(['login' => 'abc']);
+		(new Access\FacebookEntrance($this->connection, $http))->enter(['login' => 'abc']);
 	}
 
 	public function testInsertingNewFacebookAccount(): void {
-		$http = $this->mock(Client::class);
+		/** @var GuzzleHttp\Client|MockInterface $http */
+		$http = $this->mock(GuzzleHttp\Client::class);
 		$response = $this->mock(ResponseInterface::class);
 		$response->shouldReceive('getStatusCode')->andReturn(HTTP_OK);
 		$response->shouldReceive('getBody')->andReturn(Json::encode(['id' => 123, 'email' => 'new@bar.cz']));
 		$http->shouldReceive('request')->andReturn($response);
 		Assert::equal(
-			new ConstantUser(
+			new Access\ConstantUser(
 				'1',
 				[
 					'email' => 'new@bar.cz',
@@ -57,20 +55,21 @@ final class FacebookEntranceTest extends TestCase\Runtime {
 					'facebook_id' => 123,
 				]
 			),
-			(new FacebookEntrance($this->connection, $http))->enter(['login' => 'TOKEN'])
+			(new Access\FacebookEntrance($this->connection, $http))->enter(['login' => 'TOKEN'])
 		);
 	}
 
 	public function testUpdatingEmailOfAlreadyRegistered(): void {
 		[$facebookId, $email] = [123, 'old@bar.cz'];
-		(new NativeQuery($this->connection, 'INSERT INTO users (facebook_id, email) VALUES (?, ?)', [$facebookId, $email]))->execute();
-		$http = $this->mock(Client::class);
+		(new Storage\NativeQuery($this->connection, 'INSERT INTO users (facebook_id, email) VALUES (?, ?)', [$facebookId, $email]))->execute();
+		/** @var GuzzleHttp\Client|MockInterface $http */
+		$http = $this->mock(GuzzleHttp\Client::class);
 		$response = $this->mock(ResponseInterface::class);
 		$response->shouldReceive('getStatusCode')->andReturn(HTTP_OK);
 		$response->shouldReceive('getBody')->andReturn(Json::encode(['id' => $facebookId, 'email' => 'new@bar.cz']));
 		$http->shouldReceive('request')->andReturn($response);
 		Assert::equal(
-			new ConstantUser(
+			new Access\ConstantUser(
 				'1',
 				[
 					'email' => 'new@bar.cz',
@@ -81,7 +80,7 @@ final class FacebookEntranceTest extends TestCase\Runtime {
 					'facebook_id' => 123,
 				]
 			),
-			(new FacebookEntrance($this->connection, $http))->enter(['login' => 'TOKEN'])
+			(new Access\FacebookEntrance($this->connection, $http))->enter(['login' => 'TOKEN'])
 		);
 	}
 
