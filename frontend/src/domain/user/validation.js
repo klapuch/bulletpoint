f15e69d439d3bb1a804c590debb6ee30efac3c80
Dominik Klapuch <klapuchdominik@gmail.com>
2019-05-04 23:38:56 +0200

    No spaces for username

--- frontend/src/domain/user/validation.js
+++ frontend/src/domain/user/validation.js
@@ -1,20 +1,25 @@
 // @flow
 import memoize from 'memoizee';
+import { trim } from 'lodash';
 import * as validation from '../../validation';
 import type { PostedUserType, ErrorUserType } from './types';
 
 const UNKNOWN = 'UNKNOWN';
+const NO_SPACE = 'NO_SPACE';
 
 type FieldType = 'username';
 
 const MAX_USERNAME_LENGTH = 25;
 const MIN_USERNAME_LENGTH = 3;
 
+const noSpace = (value: string) => (trim(value) !== value ? NO_SPACE : null);
+
 export const errors = memoize((user: PostedUserType): ErrorUserType => ({
   username: validation.firstError([
     () => validation.required(user.username),
     () => validation.maxChars(user.username, MAX_USERNAME_LENGTH),
     () => validation.minChars(user.username, MIN_USERNAME_LENGTH),
+    () => noSpace(user.username),
   ]),
 }));
 
@@ -26,6 +31,7 @@ export const toMessage = (errors: ErrorUserType, field: FieldType) => ({
   username: {
     REQUIRED: 'Uživatelské jméno je povinné',
     MAX_CHARS: `Maximální délka je ${MAX_USERNAME_LENGTH} znaků`,
+    NO_SPACE: 'Uživatelské jméno nesmí obsahovat mezery.',
     MIN_CHARS: `Minimální délka jsou ${MIN_USERNAME_LENGTH} znaky`,
     UNKNOWN: 'Uživatelské jméno není platné',
   },
