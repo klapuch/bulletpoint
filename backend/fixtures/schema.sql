2604a44ccf369f3f6e58aa3eff2353cda726d950
Dominik Klapuch <klapuchdominik@gmail.com>
2019-05-04 14:35:16 +0200

    Typo

--- backend/fixtures/schema.sql
+++ backend/fixtures/schema.sql
@@ -737,8 +737,8 @@ BEGIN
 		FROM bulletpoint_groups
 		GROUP BY root_bulletpoint_id
 	) grouped
-	JOIN LATERAL(
-		SELECT id AS bulletpoint_id, first_value(id) over () AS root_bulletpoint_id
+	JOIN LATERAL (
+		SELECT id AS bulletpoint_id, first_value(id) OVER () AS root_bulletpoint_id
 		FROM web.bulletpoints
 		WHERE id = ANY(grouped.bulletpoint_id || grouped.root_bulletpoint_id)
 	) new_groups ON TRUE
