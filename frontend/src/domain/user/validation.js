698dfcaf8f4236b2e7d3a830a22e8c947185e1c0
Dominik Klapuch <klapuchdominik@gmail.com>
2019-05-04 23:42:56 +0200

    username validation

--- frontend/src/domain/user/validation.js
+++ frontend/src/domain/user/validation.js
@@ -6,13 +6,15 @@ import type { PostedUserType, ErrorUserType } from './types';
 
 const UNKNOWN = 'UNKNOWN';
 const NO_SPACE = 'NO_SPACE';
+const NO_EXTRA_CHARS = 'NO_EXTRA_CHARS';
 
 type FieldType = 'username';
 
 const MAX_USERNAME_LENGTH = 25;
 const MIN_USERNAME_LENGTH = 3;
 
-const noSpace = (value: string) => (trim(value) !== value ? NO_SPACE : null);
+const noSpace = (value: string) => (trim(value) === value ? null : NO_SPACE);
+const noExtraChars = (value: string) => (/^[a-zA-Z0-9_]+$/.test(value) ? null : NO_EXTRA_CHARS);
 
 export const errors = memoize((user: PostedUserType): ErrorUserType => ({
   username: validation.firstError([
@@ -20,6 +22,7 @@ export const errors = memoize((user: PostedUserType): ErrorUserType => ({
     () => validation.maxChars(user.username, MAX_USERNAME_LENGTH),
     () => validation.minChars(user.username, MIN_USERNAME_LENGTH),
     () => noSpace(user.username),
+    () => noExtraChars(user.username),
   ]),
 }));
 
@@ -32,6 +35,7 @@ export const toMessage = (errors: ErrorUserType, field: FieldType) => ({
     REQUIRED: 'Uživatelské jméno je povinné',
     MAX_CHARS: `Maximální délka je ${MAX_USERNAME_LENGTH} znaků`,
     NO_SPACE: 'Uživatelské jméno nesmí obsahovat mezery.',
+    NO_EXTRA_CHARS: 'Uživatelské jméno smí obsahovat pouze základní znaky.',
     MIN_CHARS: `Minimální délka jsou ${MIN_USERNAME_LENGTH} znaky`,
     UNKNOWN: 'Uživatelské jméno není platné',
   },
