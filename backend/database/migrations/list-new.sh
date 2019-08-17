8b4d3e9bf18acce61cd8fe76aef1a22db2bdcf00
Dominik Klapuch <klapuchdominik@gmail.com>
2019-08-17 22:12:04 +0200

    fixed shell scripts

--- backend/database/migrations/list-new.sh
+++ backend/database/migrations/list-new.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MIGRATION_FILENAMES=`find database/migrations/*/*.sql | tr '\n' ','`;
-MIGRATION_FILENAMES_TO_RUN=`psql -h localhost -U bulletpoint -d bulletpoint -tA -X -c "SELECT deploy.migrations_to_run('$MIGRATION_FILENAMES')"`;
+MIGRATION_FILENAMES=$(find database/migrations/*/*.sql | tr '\n' ',');
+MIGRATION_FILENAMES_TO_RUN=$(psql -h localhost -U bulletpoint -d bulletpoint -tA -X -c "SELECT deploy.migrations_to_run('$MIGRATION_FILENAMES')");
 
 if [[ "$MIGRATION_FILENAMES_TO_RUN" == "" ]]; then
 	echo "No migrations.";
