ca5d4e942afd8c67a361e679815d29b50af24f38
Dominik Klapuch <klapuchdominik@gmail.com>
2019-09-12 20:02:19 +0200

    Migration diff to CI

---
+++ backend/database/migrations/diff.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+BASE_DIR=/var/www/bulletpoint
+ACTUAL_SCHEMA_FILENAME=/tmp/migrations/actual.sql
+MIGRATED_SCHEMA_FILENAME=/tmp/migrations/migrated.sql
+
+mkdir -pv "$(dirname $ACTUAL_SCHEMA_FILENAME)"
+mkdir -pv "$(dirname $MIGRATED_SCHEMA_FILENAME)"
+
+pg_dump --host=127.0.0.1 --port=5432 --username=bulletpoint --no-owner --no-privileges --schema-only --format=p --file=$ACTUAL_SCHEMA_FILENAME bulletpoint
+git --no-pager diff --diff-filter=A --name-status master.. -- /var/www/bulletpoint/backend/database/migrations/**/*.sql | sed -e "s~^A[[:space:]]~$BASE_DIR/~g" | xargs --no-run-if-empty --max-lines=1 sh ${0%/*}/run.sh
+pg_dump --host=127.0.0.1 --port=5432 --username=bulletpoint --no-owner --no-privileges --schema-only --format=p --file=$MIGRATED_SCHEMA_FILENAME bulletpoint
+
+diff $ACTUAL_SCHEMA_FILENAME $MIGRATED_SCHEMA_FILENAME
+
+echo "[OK] Schemas after and before migrations are same."
