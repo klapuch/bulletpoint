874ca74ac01f2a5f0fb99b68b739100d1b8ccffb
Dominik Klapuch <klapuchdominik@gmail.com>
2019-09-12 23:51:13 +0200

    config with secrets only - split to production.ini

--- backend/App/Configuration/ApplicationConfiguration.php
+++ backend/App/Configuration/ApplicationConfiguration.php
@@ -9,25 +9,49 @@ use Klapuch\Configuration;
  * Configuration for whole application
  */
 final class ApplicationConfiguration implements Configuration\Source {
-	private const CONFIGURATION = __DIR__ . '/config.ini',
-		SECRET_CONFIGURATION = __DIR__ . '/secrets.ini',
-		ROUTES = __DIR__ . '/routes.ini';
+	private const LOCAL_CONFIGURATION = __DIR__ . '/config.local.ini';
+	private const PRODUCTION_CONFIGURATION = __DIR__ . '/config.production.ini';
+	private const SECRET_CONFIGURATION = __DIR__ . '/secrets.ini';
+	private const ROUTES = __DIR__ . '/routes.ini';
 
 	public function read(): array {
 		return (new Configuration\CachedSource(
 			new Configuration\CombinedSource(
-				new Configuration\ValidIni(new \SplFileInfo(self::CONFIGURATION)),
+				new Configuration\ValidIni(
+					new \SplFileInfo(
+						self::env() === 'local'
+							? self::LOCAL_CONFIGURATION
+							: self::PRODUCTION_CONFIGURATION,
+					),
+				),
 				new Configuration\ValidIni(new \SplFileInfo(self::SECRET_CONFIGURATION)),
 				new Configuration\NamedSource(
 					'ROUTES',
 					new Configuration\ValidIni(new \SplFileInfo(self::ROUTES)),
 				),
 			),
-			$this->key(),
+			self::key(),
 		))->read();
 	}
 
-	private function key(): string {
-		return (string) crc32(implode(',', array_map('filemtime', [self::CONFIGURATION, self::SECRET_CONFIGURATION, self::ROUTES])));
+	private static function env(): string {
+		return $_SERVER['BULLETPOINT_ENV'] ?? 'production';
+	}
+
+	private static function key(): string {
+		return (string) crc32(
+			implode(
+				',',
+				array_map(
+					'filemtime',
+					[
+						self::LOCAL_CONFIGURATION,
+						self::PRODUCTION_CONFIGURATION,
+						self::SECRET_CONFIGURATION,
+						self::ROUTES,
+					],
+				),
+			),
+		);
 	}
 }
