fc74736a964d0f87d40a9ed5ca343edb5df7190e
Dominik Klapuch <klapuchdominik@gmail.com>
2019-09-21 18:18:02 +0200

    Better migration diff

--- backend/database/migrations/diff.sh
+++ backend/database/migrations/diff.sh
@@ -2,15 +2,28 @@
 set -e
 
 BASE_DIR=/var/www/bulletpoint
+TMP_DATABASE_NAME=tmp_bulletpoint
 ACTUAL_SCHEMA_FILENAME=/tmp/migrations/actual.sql
 MIGRATED_SCHEMA_FILENAME=/tmp/migrations/migrated.sql
+MASTER_SCHEMA_FILENAME=/tmp/migrations/master.sql
 
 mkdir -pv "$(dirname $ACTUAL_SCHEMA_FILENAME)"
 mkdir -pv "$(dirname $MIGRATED_SCHEMA_FILENAME)"
+mkdir -pv "$(dirname $MASTER_SCHEMA_FILENAME)"
 
-pg_dump --host=127.0.0.1 --port=5432 --username=bulletpoint --no-owner --no-privileges --schema-only --format=p --file=$ACTUAL_SCHEMA_FILENAME bulletpoint
-git --no-pager diff --diff-filter=A --name-only master.. -- $BASE_DIR/backend/database/migrations/**/*.sql | xargs --no-run-if-empty printf "$BASE_DIR/%s\n" | xargs --no-run-if-empty --max-lines=1 sh ${0%/*}/run.sh
-pg_dump --host=127.0.0.1 --port=5432 --username=bulletpoint --no-owner --no-privileges --schema-only --format=p --file=$MIGRATED_SCHEMA_FILENAME bulletpoint
+psql -U bulletpoint -h 127.0.0.1 -d postgres -c "DROP DATABASE IF EXISTS $TMP_DATABASE_NAME";
+psql -U bulletpoint -h 127.0.0.1 -d postgres -c "CREATE DATABASE $TMP_DATABASE_NAME";
+psql -U bulletpoint -h 127.0.0.1 $TMP_DATABASE_NAME < $BASE_DIR/backend/database/schema.sql;
+pg_dump --host=127.0.0.1 --port=5432 --username=bulletpoint --no-owner --no-privileges --schema-only --format=p --file=$ACTUAL_SCHEMA_FILENAME $TMP_DATABASE_NAME
+
+psql -U bulletpoint -h 127.0.0.1 -d postgres -c "DROP DATABASE $TMP_DATABASE_NAME";
+psql -U bulletpoint -h 127.0.0.1 -d postgres -c "CREATE DATABASE $TMP_DATABASE_NAME";
+git --no-pager show "$(git merge-base master HEAD):backend/database/schema.sql" > $MASTER_SCHEMA_FILENAME;
+psql -U bulletpoint -h 127.0.0.1 $TMP_DATABASE_NAME < $MASTER_SCHEMA_FILENAME;
+git --no-pager diff --diff-filter=A --name-only master.. -- $BASE_DIR/backend/database/migrations/**/*.sql \
+  | xargs --no-run-if-empty -I {} psql -h 127.0.0.1 -U bulletpoint --single-transaction -d $TMP_DATABASE_NAME -v ON_ERROR_STOP=1 -X --file $BASE_DIR/{}
+
+pg_dump --host=127.0.0.1 --port=5432 --username=bulletpoint --no-owner --no-privileges --schema-only --format=p --file=$MIGRATED_SCHEMA_FILENAME $TMP_DATABASE_NAME
 
 diff $ACTUAL_SCHEMA_FILENAME $MIGRATED_SCHEMA_FILENAME
 
